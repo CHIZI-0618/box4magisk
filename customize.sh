f871e3ebc0ef7cf8948b442256d00f292dfc34d4
CHIZI-0618 <kay9925@outlook.com>
2022-11-22 16:34:59 +0800

    Add custom user group startup

--- customize.sh
+++ customize.sh
@@ -34,10 +34,11 @@ mv -f $MODPATH/box4magisk_service.sh /data/adb/service.d/
 rm -f customize.sh
 
 set_perm_recursive $MODPATH 0 0 0755 0644
+set_perm_recursive $MODPATH/system/bin/ 0 0 0755 0755
 set_perm_recursive /data/adb/box/ 0 3005 0755 0644
 set_perm_recursive /data/adb/box/scripts/ 0 3005 0755 0700
 
 set_perm /data/adb/service.d/box4magisk_service.sh 0 0 0700
 
-#fix "set_perm_recursive  /data/adb/sing-box/scripts" not working on some phones. It didn't work on my Oneplus 7 pro and Remi K50.
+#fix "set_perm_recursive /data/adb/box/scripts" not working on some phones. It didn't work on my Oneplus 7 pro and Remi K50.
 chmod ugo+x /data/adb/box/scripts/*
