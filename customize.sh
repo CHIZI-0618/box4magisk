28c85e6cf1328ab92313a6b9cd36aec9645adec9
CHIZI-0618 <kay9925@outlook.com>
2022-11-24 15:19:27 +0800

    Remove the installation of libcap arm64

--- customize.sh
+++ customize.sh
@@ -34,9 +34,8 @@ mv -f $MODPATH/box4magisk_service.sh /data/adb/service.d/
 rm -f customize.sh
 
 set_perm_recursive $MODPATH 0 0 0755 0644
-set_perm_recursive $MODPATH/system/bin/ 0 0 0755 0755
-set_perm_recursive /data/adb/box/ 0 3005 0755 0644
-set_perm_recursive /data/adb/box/scripts/ 0 3005 0755 0700
+set_perm_recursive /data/adb/box/ 0 0 0755 0644
+set_perm_recursive /data/adb/box/scripts/ 0 0 0755 0700
 
 set_perm /data/adb/service.d/box4magisk_service.sh 0 0 0700
 
