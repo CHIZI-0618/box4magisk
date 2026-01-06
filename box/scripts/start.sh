bc40318d50edaae680613f0b9ebc5f643c9e7423
CHIZI-0618 <51962278+CHIZI-0618@users.noreply.github.com>
2026-01-06 14:15:51 +0800

    refactor(tproxy)! (#129)
    * refactor(tproxy)!
    
    BREAKING CHANGE: Configuration format changed - migration required
    
    References:
    https://github.com/CHIZI-0618/AndroidTProxyShell/blob/main/tproxy.sh
    
    * Update README
    
    Thanks to:
    - Grok (xAI) for writing the Chinese documentation
    - ChatGPT (OpenAI) for translating to English
    
    * Update v5.2pre
    
    * shfmt
    
    shfmt -i=4 -bn -ci -sr -w box/scripts/*

--- box/scripts/start.sh
+++ box/scripts/start.sh
@@ -9,19 +9,19 @@ scripts_dir=$(dirname ${scripts})
 
 source ${scripts_dir}/box.config
 
-wait_until_login(){
-  # we doesn't have the permission to rw "/sdcard" before the user unlocks the screen
-  local test_file="/sdcard/Android/.BOX4TEST"
-  true > "$test_file"
-  while [ ! -f "$test_file" ] ; do
+wait_until_login() {
+    # we doesn't have the permission to rw "/sdcard" before the user unlocks the screen
+    local test_file="/sdcard/Android/.BOX4TEST"
     true > "$test_file"
-    sleep 1
-  done
-  rm "$test_file"
+    while [ ! -f "$test_file" ]; do
+        true > "$test_file"
+        sleep 1
+    done
+    rm "$test_file"
 
-  while [ ! -f "/data/system/packages.xml" ] ; do
-    sleep 1
-  done
+    while [ ! -f "/data/system/packages.xml" ]; do
+        sleep 1
+    done
 }
 
 wait_until_login
@@ -29,10 +29,10 @@ wait_until_login
 rm ${pid_file}
 mkdir -p ${run_path}
 
-if [ ! -f ${box_path}/manual ] && [ ! -f ${module_dir}/disable ] ; then
-  mv ${run_path}/run.log ${run_path}/run.log.bak
-  mv ${run_path}/run_error.log ${run_path}/run_error.log.bak
+if [ ! -f ${box_path}/manual ] && [ ! -f ${module_dir}/disable ]; then
+    mv ${run_path}/run.log ${run_path}/run.log.bak
 
-  ${scripts_dir}/box.service start >> ${run_path}/run.log 2>> ${run_path}/run_error.log && \
-  ${scripts_dir}/box.tproxy enable >> ${run_path}/run.log 2>> ${run_path}/run_error.log
+    ${scripts_dir}/box.service start >> /dev/null 2>> ${run_path}/run.log \
+        && ${scripts_dir}/box.tproxy start >> /dev/null 2>> ${run_path}/run.log
+
 fi
